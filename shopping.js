399e668d7f7f232861cf5bec3c692f34a07638a6
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-05-27 18:54:53 -0500

    created an array of shopping list objects

---
+++ shopping.js
@@ -0,0 +1,39 @@
+const shoppingList = [
+    {
+        id: 1,
+        name: "Mango juice",
+        price: "$5"
+    },
+    {
+        id: 2,
+        name: "Kimchi",
+        price: "$7"
+    },
+    {
+        id: 3,
+        name: "Jasmine Rice",
+        price: "$25"
+    },
+    {
+        id: 4,
+        name: "Beef",
+        price: "$10"
+    },
+    {
+        id: 5,
+        name: "Chocolate",
+        price: "$3"
+    },
+    {
+        id: 6,
+        name: "Chips",
+        price: "$4"
+    },
+    {
+        id: 7,
+        name: "Ice Cream",
+        price: "6"
+    }
+];
+
+console.log(shoppingList);
